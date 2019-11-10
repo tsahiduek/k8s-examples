3218021f44bb62cb9148d0ff9808058b0639ff18
Tsahi Duek <tsahiduek@gmail.com>
2019-11-10 22:33:28 +0200

    validating examples + cheatsheet for cncf slides demo

---
+++ scheduling-examples/cncf-kubectl-commands.sh
@@ -0,0 +1,30 @@
+# get all nodes
+kn -Lbeta.kubernetes.io/instance-type -Lfailure-domain.beta.kubernetes.io/zone
+
+# deploy pod with nodeSelector
+k apply -f nodeselector-instance-type.yaml
+
+# deploy pod with nodeAffinity
+k apply -f nodeselector-in-nodeaffinity.yaml
+
+# deploy 2 pods in the same host
+k apply -f pod-affinity.yaml
+k get pods -lservice=core -o wide
+
+# deploy 1 pod not with service=core
+k apply -f pod-antiaffinity.yaml
+
+# deploy 1 pod per host
+k apply -f deployment-ha-host.yaml
+
+# deploy 1 pod per host spread AZ
+k apply -f deployment-ha-host-zone.yaml
+
+
+# cleanup
+k delete -f nodeselector-instance-type.yaml
+k delete -f nodeselector-in-nodeaffinity.yaml
+k delete -f pod-affinity.yaml
+k delete -f pod-antiaffinity.yaml
+k delete -f deployment-ha-host.yaml
+k delete -f deployment-ha-host-zone.yaml
